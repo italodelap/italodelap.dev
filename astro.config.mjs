b30f9d8531abe1bf00cacf0f928e6dba8e069d86
Italo De la Peña <italo.delapena@utdt.edu>
2024-08-19 11:23:26 -0300

    feat(*): ➕ install `tailwind-animated`

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,6 +4,6 @@ import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
 export default defineConfig({
-  site: "https://www.gaterapia.com",
+  site: "https://www.italodelap.dev",
   integrations: [tailwind()]
 });
