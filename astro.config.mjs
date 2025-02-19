a9d04cd87f617eb3ded8e0a770f774fc158eee2f
Italo De la Peña <italo.delapena@utdt.edu>
2025-02-19 15:47:21 -0300

    feat(*): ⬆️ upgrade Tailwind to v4

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,11 @@
 import { defineConfig } from 'astro/config';
 
-import tailwind from "@astrojs/tailwind";
+import tailwindcss from "@tailwindcss/vite";
 
 // https://astro.build/config
 export default defineConfig({
   site: "https://www.italodelap.dev",
-  integrations: [tailwind()]
+  vite: {
+    plugins: [tailwindcss()],
+  },
 });
