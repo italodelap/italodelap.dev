5023fccb48f7f9e1779cff0d913bd71153d9c531
Italo De la Peña <italo.delapena@utdt.edu>
2025-02-25 12:51:14 -0300

    feat(/resume): ✨ start resume development

--- src/lib/dates.ts
+++ src/lib/dates.ts
@@ -1,3 +1,5 @@
+import { experience } from "@/config/site.json";
+
 function getDateDifferenceInMonths(initialDate: Date, finalDate: Date): number {
   return Math.max(
     (finalDate.getFullYear() - initialDate.getFullYear()) * 12 +
@@ -7,6 +9,20 @@ function getDateDifferenceInMonths(initialDate: Date, finalDate: Date): number {
   );
 }
 
-export function getDateDifferenceInYears(initialDate: Date, finalDate: Date): number {
+function getDateDifferenceInYears(initialDate: Date, finalDate: Date): number {
   return getDateDifferenceInMonths(initialDate, finalDate) / 12;
+}
+
+export function getExperienceYearsAmount() {
+  const myFirstJobAsDeveloper = experience[0].subitems![1];
+  const [year, month, day] = myFirstJobAsDeveloper.from
+    .split("-")
+    .map(Number)
+    .map((date, index) => (index === 1 ? date - 1 : date));
+
+  const startDateOfMyWorkExperience = new Date(year, month, day);
+
+  return Math.trunc(
+    getDateDifferenceInYears(startDateOfMyWorkExperience, new Date()),
+  );
 }
