4cc10e1272ee587b26b5b068c9efca094cabb214
Italo De la Peña <italo.delapena@utdt.edu>
2025-03-09 17:04:09 -0300

    feat(*): 📈 add web analytics

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,11 @@
 import { defineConfig } from 'astro/config';
 
+import vercel from "@astrojs/vercel";
 import tailwindcss from "@tailwindcss/vite";
 
 // https://astro.build/config
 export default defineConfig({
+  adapter: vercel({ webAnalytics: { enabled: true } }),
   site: "https://www.italodelap.dev",
-  vite: {
-    plugins: [tailwindcss()],
-  },
+  vite: { plugins: [tailwindcss()] },
 });
