8ea0f68aef6ea381c15ade8c3f2f4b1b9c546e45
Italo De la Peña <italo.delapena@utdt.edu>
2025-03-10 01:39:40 -0300

    fix(*): 📈 re-configure Vercel analytics

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,12 +1,10 @@
 import { defineConfig } from 'astro/config';
 
 import sitemap from "@astrojs/sitemap";
-import vercel from "@astrojs/vercel";
 import tailwindcss from "@tailwindcss/vite";
 
 // https://astro.build/config
 export default defineConfig({
-  adapter: vercel({ webAnalytics: { enabled: true } }),
   integrations: [sitemap()],
   site: "https://www.italodelap.dev",
   vite: { plugins: [tailwindcss()] },
